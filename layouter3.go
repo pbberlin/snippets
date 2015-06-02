f6aa3386bd846db029ccc797d155cdcdce7ee8e0
Peter Buchmann <peter.buchmann@web.de>
2015-06-02 16:58:36 +0200

    minor

--- layouter3.go
+++ layouter3.go
@@ -5,7 +5,7 @@ import "github.com/pbberlin/tools/transposablematrix"
 func main() {
 
 	transposablematrix.InitTB()
-	transposablematrix.Main4()
+	transposablematrix.Main3()
 	// transposablematrix.MainTestNoEasternNeighbor()
 	// transposablematrix.MainStairyShrinky()
 
