22b20d70427bd17e7624fb169fca011904b76d01
Peter Buchmann <peter.buchmann@web.de>
2015-06-06 11:17:24 +0200

    intermed

--- parse/main.go
+++ parse/main.go
@@ -6,13 +6,12 @@ import (
 	"log"
 	"strings"
 
-	"github.com/pbberlin/tools/fetch"
 	"golang.org/x/net/html"
 )
 
 func main() {
 
-	s := `	<p>Links:
+	s1 := `	<p>Links:
 				<span>p1</span>
 				<span>p2</span>
 			</p>
@@ -20,26 +19,39 @@ func main() {
 				<li id='332' ><a   href="foo">Linktext1 <span>inside</span></a>
 				<li><a   href="/bar/baz">BarBaz</a>
 			</ul>`
+	s2 := `	<p>
+				Ja so sans<br/>
+				Ja die sans.
+			</p>
+			<ul>
+				<li>die ooolten Rittersleut</li>
+			</ul>`
 
 	var doc1, doc2 *html.Node
 	_, _ = doc1, doc2
 	var err error
 
-	doc1, err = html.Parse(strings.NewReader(s))
+	doc1, err = html.Parse(strings.NewReader(s1))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, resBytes, err := fetch.UrlGetter("http://localhost:4000/static/handelsblatt.com/article01.html", nil, true)
-	doc2, err = html.Parse(bytes.NewReader(resBytes))
+	doc2, err = html.Parse(strings.NewReader(s2))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// _, resBytes, err := fetch.UrlGetter("http://localhost:4000/static/handelsblatt.com/article01.html", nil, true)
+	// doc3, err = html.Parse(bytes.NewReader(resBytes))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	TraverseHori(Tx{doc1, 0})
 
 	TraverseVert(doc1, 0)
 
+	//
 	ioutil.WriteFile("outp.txt", stackOutp, 0)
 
 	// to file
